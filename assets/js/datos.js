e740db4a47dd73a177f5c81d78ba803d2cff038b
anibal317 <anibal317@hotmail.com>
2020-12-10 19:21:08 -0300

    Proyecto final

---
+++ assets/js/datos.js
@@ -0,0 +1,37 @@
+let itemsTable = [
+    {
+        id: 1,
+        mes: "Enero",
+        anio: 2020,
+        start: 10000,
+        end: 15000,
+    },
+    {
+        id: 2,
+        mes: "Febrero",
+        anio: 2020,
+        start: 15000,
+        end: 15200,
+    },
+    {
+        id: 3,
+        mes: "Marzo",
+        anio: 2020,
+        start: 15200,
+        end: 16200,
+    },
+    {
+        id: 4,
+        mes: "Abril",
+        anio: 2020,
+        start: 16200,
+        end: 17200,
+    },
+    {
+        id: 5,
+        mes: "Mayo",
+        anio: 2020,
+        start: 17200,
+        end: 18500,
+    }
+];
---
+++ assets/js/getDomElements.js
@@ -0,0 +1,20 @@
+let btnAdd = document.getElementById("btnAdd");
+let barrita = document.getElementById("barrita");
+let modalBarrita = document.getElementById("progessBarModal");
+let upBarrita = document.getElementById("llamarABarrita");
+let btnEditElement = document.getElementsByClassName("acciones");
+
+let editMonth = document.getElementById("edit-month");
+let editYear = document.getElementById("edit-year");
+let editInitialAmount = document.getElementById("edit-initialAmount");
+let editFinalAmount = document.getElementById("edit-finalAmount");
+
+let tbody = document.getElementsByTagName("tbody")[0];
+
+
+let filter = document.getElementById("activeFilter");
+let showFilter = document.getElementById("showFilters");
+
+let btnBuscar = document.getElementById("btnBuscar");
+let inputSearch = document.getElementById("searchByFilter");
+let listFilters = document.getElementById("filters");
